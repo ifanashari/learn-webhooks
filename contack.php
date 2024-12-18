96a426717504b946c465f235dc2f68f4f4db6f75
unknown <ifanashari1234@gmail.com>
2024-12-18 21:53:01 +0700

    initial commit

---
+++ contack.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kontak</title>
+    <style>
+        .nomer {
+            display: flex;
+            justify-content: center;
+            margin-top: 250px;
+        }
+        .nomer img {
+            width: 100px;
+        }
+        .nomer h1 {
+            align-items: center;
+        }
+    </style>
+</head>
+<body>
+    <div class="nomer">
+        <img src="assets/image/warill.png" alt="">
+        <h1>085782903761</h1>
+    </div>
+    
+</body>
+</html>
